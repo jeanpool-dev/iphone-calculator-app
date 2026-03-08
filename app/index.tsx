e33f0ffc52c98f2b07a28bdbf3eabc54c2e30cb4
Jean Pool Gutierrez <jeanpool.dev@hotmail.com>
2026-03-08 17:08:18 -0500

    Estilos globales

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,11 @@
+import { globalStyles } from "@/styles/global-styles";
 import { Text, View } from "react-native";
 
 const IPhoneCalculatorApp = () => {
   return (
-    <View>
-      <Text style={{ fontSize: 50, fontFamily: "SpaceMono", color: "white" }}>
-        IPhoneCalculatorApp
-      </Text>
+    <View style={globalStyles.calculatorContainer}>
+      <Text style={globalStyles.mainResult} numberOfLines={1} adjustsFontSizeToFit>50 x 50000000000000000000</Text>
+      <Text style={globalStyles.subResult}>2500</Text>
     </View>
   );
 };

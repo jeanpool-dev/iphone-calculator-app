10a3d4b5edd6b8e0921a9a59b7ebab8152442ae0
Jean Pool Gutierrez <jeanpool.dev@hotmail.com>
2026-03-08 15:36:31 -0500

    ExpoRouter: index y _layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,14 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
+import { Slot } from "expo-router";
+import { Text, View } from "react-native";
 
-import { useColorScheme } from '@/hooks/use-color-scheme';
-
-export const unstable_settings = {
-  anchor: '(tabs)',
+const RootLayout = () => {
+  return (  
+    <View>
+      <Text>Header</Text>
+      <Slot />
+      <Text>Footer</Text>
+    </View>
+  );
 };
 
-export default function RootLayout() {
-  const colorScheme = useColorScheme();
-
-  return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
-  );
-}
+export default RootLayout;

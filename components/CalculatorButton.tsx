cf3f185d6a365a690ecef1ba70b10e0acd2ec087
Jean Pool Gutierrez <jeanpool.dev@hotmail.com>
2026-03-15 14:26:59 -0500

    feat: add haptic feedback on press

--- components/CalculatorButton.tsx
+++ components/CalculatorButton.tsx
@@ -1,5 +1,6 @@
 import { Colors } from "@/constants/theme";
 import { globalStyles } from "@/styles/global-styles";
+import * as Haptics from "expo-haptics";
 import { Pressable, Text } from "react-native";
 
 interface Props {
@@ -25,7 +26,10 @@ const CalculatorButton = ({
         opacity: pressed ? 0.8 : 1,
         width: doubleSize ? 180 : 80,
       })}
-      onPress={onPress}
+      onPress={() => {
+        Haptics.notificationAsync();
+        onPress();
+      }}
     >
       <Text
         style={{
